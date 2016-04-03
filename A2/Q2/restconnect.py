abc53a0814f864e994d1cb470ac8949fc569f3f3
Divyanshu Shende <divush@cse.iitk.ac.in>
2016-04-03 20:09:46 +0530

    Assignment 3 done!

--- A2/Q2/restconnect.py
+++ A2/Q2/restconnect.py
@@ -1,10 +1,11 @@
 import sklearn, nltk
 from sklearn import svm
+from sklearn.metrics import accuracy_score
 import numpy as np
 import sys
-
+import copy
 fp=open('connect-4.data', 'r')
-data=fp.split()
+data=fp.read().strip().split()
 fp.close()
 
 #Vectorize data.
@@ -23,14 +24,97 @@ for x in data:
 	for stuff in y:
 		if stuff == 'x':
 			temp.append(1)
-		elif stuff == 'o':
-			temp.append(-1)
 		elif stuff == 'b':
 			temp.append(0)
+		elif stuff == 'o':
+			temp.append(-1)
 	Game.append(temp)
 
 #Vectorization of data complete! Game contains feature vectors and Outcome contains the outcomes
+VGame=[[],[],[],[],[],[]]
+VOutcome=[[],[],[],[],[],[]]
+l = len(Game)
+vl=int(l/5)+1
+for i in range(l):
+	t=int(i/vl)
+	x=Game[i]
+	y=Outcome[i]
+	VGame[t].append(x)
+	VOutcome[t].append(y)
 
-trainwin=[]
-trainloss=[]
-traindraw=[]
+clfw = svm.LinearSVC(loss='hinge')
+clfd = svm.LinearSVC(loss='hinge')
+clfl = svm.LinearSVC(loss='hinge')
+
+#create learning sets for 3 classifiers
+for vpart in range(5):
+	# print("Validating part "+str(vpart))
+	#win classifier
+	gw=[]
+	ow=[]
+	#draw classifier
+	gd=[]
+	od=[]
+	#loss classifier
+	gl=[]
+	ol=[]
+
+	#Extract Test data
+	Pred=[]
+	OTest=copy.deepcopy(VOutcome[vpart])
+	GTest=copy.deepcopy(VGame[vpart])
+	#Create Training sets for 3 classifiers!
+	for z in range(5):
+		# print("z = "+str(z))
+		if z == vpart:
+			continue
+		else:
+			# print(len(VGame[z]))
+			O=copy.deepcopy(VOutcome[z])
+			G=copy.deepcopy(VGame[z])
+			# print(str(len(VGame[z]))+" "+str(len(G)))
+			for i in range(len(O)):
+				if O[i] == 1:
+					gw.append(G[i])
+					gd.append(G[i])
+					gl.append(G[i])
+					ow.append(1)
+					od.append(0)
+					ol.append(0)
+				if O[i] == 0:
+					gw.append(G[i])
+					gd.append(G[i])
+					gl.append(G[i])
+					ow.append(0)
+					od.append(1)
+					ol.append(0)
+				if O[i] == -1:
+					gw.append(G[i])
+					gd.append(G[i])
+					gl.append(G[i])
+					ow.append(0)
+					od.append(0)
+					ol.append(1)
+	clfw.fit(gw,ow)
+	clfd.fit(gd,od)
+	clfl.fit(gl,ol)
+	Pred1 = clfw.decision_function(GTest)
+	Pred2 = clfl.decision_function(GTest)
+	Pred3 = clfd.decision_function(GTest)
+	temparr.append(Pred1)
+	for x in range(len(Pred1)):
+		temparr=[]
+		temparr.append(Pred1[x])
+		temparr.append(Pred2[x])
+		temparr.append(Pred3[x])
+		# print(temparr)
+		maxarg = np.argmax(temparr)
+		if maxarg==0:
+			Pred.append(1)
+		if maxarg==1:
+			Pred.append(0)
+		if maxarg==2:
+			Pred.append(-1)
+
+	accuracy = accuracy_score(OTest, Pred)
+	print(accuracy)
---
+++ A3/randomforestclf.py
@@ -0,0 +1,68 @@
+import sklearn, sys, time, skimage, os, struct
+from sklearn import tree
+from skimage import data, color, exposure
+import numpy as np
+from sklearn.datasets.mldata import fetch_mldata
+from array import array as pyarray
+from numpy import append, array, int8, uint8, zeros
+from skimage.feature import hog
+from sklearn.ensemble import RandomForestClassifier
+def load_mnist(dataset="training", digits=np.arange(10), path="."):
+    """
+    Loads MNIST files into 3D numpy arrays
+
+    Adapted from: http://abel.ee.ucla.edu/cvxopt/_downloads/mnist.py
+    """
+
+    if dataset == "training":
+        fname_img = os.path.join(path, 'mnist-train-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 'mnist-train-labels-idx1-ubyte')
+    elif dataset == "testing":
+        fname_img = os.path.join(path, 'mnist-t10k-images-idx3-ubyte')
+        fname_lbl = os.path.join(path, 'mnist-t10k-labels-idx1-ubyte')
+    else:
+        raise ValueError("dataset must be 'testing' or 'training'")
+
+    flbl = open(fname_lbl, 'rb')
+    magic_nr, size = struct.unpack(">II", flbl.read(8))
+    lbl = pyarray("b", flbl.read())
+    flbl.close()
+
+    fimg = open(fname_img, 'rb')
+    magic_nr, size, rows, cols = struct.unpack(">IIII", fimg.read(16))
+    img = pyarray("B", fimg.read())
+    fimg.close()
+    if dataset== "training":
+        size = 10000
+    else:
+        size = 1000
+    ind = [ k for k in range(size) if lbl[k] in digits ]
+    N = len(ind)
+
+    images = zeros((N, rows, cols), dtype=uint8)
+    labels = zeros((N, 1), dtype=int8)
+    for i in range(len(ind)):
+        images[i] = array(img[ ind[i]*rows*cols : (ind[i]+1)*rows*cols ]).reshape((rows, cols))
+        labels[i] = lbl[ind[i]]
+
+    return images, labels
+
+digs= [0,1,2,3,4,5,6,7,8,9]
+X_tr, y_train = load_mnist('training', digits=digs)
+X_te, y_test = load_mnist('testing', digits=digs)
+X_train=[]
+X_test=[]
+for img in X_tr:
+	# print(len(img))
+	# print(len(img[0]))
+	temp2 = hog(img)
+	X_train.append(temp2)
+for img in X_te:
+	temp2 = hog(img)
+	X_test.append(temp2)
+nt=500
+clf = RandomForestClassifier(n_estimators = nt, max_depth=4)
+clf.fit(X_train, y_train, sample_weight=None)
+test_out=clf.predict(X_test)
+scor=clf.score(X_test, y_test)
+print("number of trees = "+str(nt)+ "\tscore = "+str(scor)) #printing correct/total
